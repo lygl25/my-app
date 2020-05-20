fd2798de04554725e39ee7656d890fdfe40820f7
lygl25 <lygl25@163.com>
2020-05-20 17:41:23 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+ 
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="./assets/phaser3-logo-nav.png"
+          width="100"
+        /><v-toolbar-title>极客中文站</v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn
+        href=""
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Phaser3例子</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,149 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12" align="center">
+        <v-img
+          :src="require('../assets/logo.png')"
+          class="my-3"
+          contain
+          height="545"
+          width="635"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Phaser3 极客中文站
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Phaser是一个快速，免费且有趣的开源HTML5游戏框架，可在台式机和移动Web浏览器上提供WebGL和Canvas渲染,
+          <br>可以使用第三方工具将游戏编译为iOS，Android和本机应用程序。您可以使用JavaScript或TypeScript进行开发。
+          <a
+            href="http://phaser.io/"
+            target="_blank"
+          >官方网站</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          关于本站
+        </h2>
+
+        <p class="subheading font-weight-regular">
+          本站镜像官方Phaser3的示例程序、API文档实现在国内的快速访问，
+          <br>方便国内Phaser爱好者快速查阅官方资料。
+      
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          最新官方资料(英文原版镜像)
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+
+         
+
+      </v-col>
+            <v-col
+      align="center"
+        class="mb-5"
+        cols="12"
+      >
+           <h2 class="headline font-weight-bold mb-3">
+          展示
+        </h2>
+              <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks2"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+
+       </v-col>
+      <v-col
+      align="center"
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          成为支持者
+        </h2>
+           <p class="subheading font-weight-regular">
+         这些资金使Phaser3极客中文站得以改善，并且在改善时，每个人都将从中受益。
+          <br>您的支持有助于确保持续不断的更新，修复，新功能以及对未来的计划。
+         </p>
+        <p> 支付宝捐赠：
+用手机扫描二位码支付</p>
+ <v-img
+          :src="require('../assets/aipay.png')"
+          class="my-3"
+          contain
+          height="200"
+          width="201"
+        />
+        <v-row justify="center">
+
+        </v-row>
+
+      </v-col>
+    
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+
+      importantLinks: [
+        {
+          text: 'Phaser3实例',
+          href: 'https://www.jikeyt.com/page/phaser/phaser3-examples/public/index.html',
+        },
+        {
+          text: 'Phaser3 API文档',
+          href: 'https://www.jikeyt.com/page/phaser/phaser3-docs/docs/index.html',
+        }
+      ],
+            importantLinks2: [
+        {
+          text: '俄罗斯方块',
+          href: 'https://www.jikeyt.com/page/game/vue-tetris/index.html',
+        },
+        {
+          text: 'Phaser3入门小游戏',
+          href: 'https://www.jikeyt.com/page/game/phaser3_test/index.html',
+        }
+      ]
+    
+    }),
+  }
+</script>
